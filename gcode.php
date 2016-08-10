e3e49111a2ad3d4c07408986239143d4ca6300d1
Flashsolutions <info@flashsolutions.us>
2016-08-10 09:01:12 -0400

    Bug fixes and added Pass count option
    Now you can specify multiple passes.

--- gcode.php
+++ gcode.php
@@ -1,6 +1,6 @@
 <?php
 
-function ImageFlip ( $imgsrc, $mode )
+function ImageFliper ( $imgsrc, $mode )
 {
 
     $width                        =    imagesx ( $imgsrc );
@@ -59,11 +59,11 @@ function map($value, $fromLow, $fromHigh, $toLow, $toHigh) {
     return $tmpValue + $toLow;
 }
 
-set_time_limit(300);
+set_time_limit(3000);   // Do we need this?  What if the job takes longer than this to print?
 
 //error_reporting(E_ALL);
 //ini_set('display_errors', 1);
-
+date_default_timezone_set('America/New_York');
 $now = new DateTime();
 $name = $now->getTimestamp();           // Unix Timestamp -- Since PHP 5.3;
 $ext = "jpg";
@@ -118,13 +118,12 @@ if(!isset($_POST['sizeY']) || $_POST['sizeY'] == 0)
    exit();
    }
 
-//header('Content-Type: text/plain; charset=utf-8');
 
-
-$laserMax=$_POST['LaserMax'];//$laserMax=65; //out of 255
-$laserMin=$_POST['LaserMin']; //$laserMin=20; //out of 255
-$laserOff=$_POST['LaserOff'];//$laserOff=13; //out of 255
-$laserCMD=$_POST['LaserCmd'];//$laserCMD=M42 P4 S or laserCMD=M106 S for example
+$laserMax=$_POST['LaserMax'];         //$laserMax=65; //out of 255
+$laserMin=$_POST['LaserMin'];         //$laserMin=20; //out of 255
+$laserOFFPwr=$_POST['laserOFFPwr'];   //$laserOff=13; //out of 255
+$laserONCmd=$_POST['LaserONCmd'];     //$laserCMD=M42 P4 S or laserCMD=M106 S or M3 S for example
+$laserOFFCmd=$_POST['LaserOFFCmd'];   // M5 or M3 S for example
 $whiteLevel=$_POST['whiteLevel'];
 
 $feedRate = $_POST['feedRate'];//$feedRate = 800; //in mm/sec
@@ -141,6 +140,7 @@ $sizeX=$sizeY*$w/$h; //SET A HEIGHT AND CALC WIDTH (this should be customizable)
 $resX=$_POST['resX'];//$resX=.1;
 
 $passCnt=$_POST['passCnt']; //$passCnt 1 or greater
+$passDepth=$_POST['passDepth']; // passDepth 1 mm
 
 $pixelsX = round($sizeX/$resX);
 $pixelsY = round($sizeY/$scanGap);
@@ -149,10 +149,10 @@ $tmp = imagecreatetruecolor($pixelsX, $pixelsY);
 imagecopyresampled($tmp, $src, 0, 0, 0, 0, $pixelsX, $pixelsY, $w, $h);
 
 $fliped = $tmp;
-$tmp = ImageFlip($tmp,3);
+$tmp = ImageFliper($tmp,3);
 if($_POST['flip'] != 1) 
   {
-    $tmp = ImageFlip($tmp,2);
+    $tmp = ImageFliper($tmp,2);
   }
 
 
@@ -176,77 +176,104 @@ if($_POST['preview'] == 1)
 
 
 header("Content-Disposition: attachment; filename=".$_FILES['image']['name'].".gcode");
+// Let's make sure the Laser is off before we get started!!
+print("$laserOFFCmd; Turn laser off\n");
 
 print(";Created using Nebarnix's IMG2GCO program Ver 1.0 with modifications by Larry Fortna\n");
-print(";http://gcode.flashsolutions.us/img2gcode/index.html\n");
+print(";http://gcode.flashsolutions.us/img2gcode/V2/index.html\n");
 print(";Image Filename:".$_FILES['image']['name'] );
 print(";Size in pixels X=$pixelsX, Y=$pixelsY\n");
 $cmdRate = round(($feedRate/$resX)*2/60);
 print(";Speed is $feedRate mm/min, $resX mm/pix => $cmdRate lines/min\n");
-print(";Power is $laserMin to $laserMax (". round($laserMin/255*100,1) ."%-". round($laserMax/255*100,1) ."%)\n");
+print(";Power is $laserMin to $laserMax (". round($laserMin/100,1) ."%-". round($laserMax/100,1) ."%)\n");
 print(";Pass count value input is $passCnt\n");
 
-print("G21\n");
-print("$laserCMD$laserOff; Turn laser off\n");
+print("G21 ;Set Units to MM\n");
+print("G90 ;Absolute positioning\n");  // use absolute positioning 
+print("G28 ;Go HOME\n");
 print("G1 F$feedRate\n");
 
-$lineIndex=0;
+
 print("G0 X$offsetX Y$offsetY F$travelRate\n");
 
 for($pass=1; $pass<($passCnt+1); $pass++) 
 {
+  $lineIndex=0;
   print(";PASS $pass\n");
   for($line=$offsetY; $line<($sizeY+$offsetY); $line+=$scanGap)
   {  
      //analyze the row and find first and last nonwhite pixels
      $pixelIndex=0;
-     $firstX = 0;
-     $lastX = 0;
-     for($pixelIndex=0; $pixelIndex < $pixelsX; $pixelIndex++)
+     $firstX=0;
+     $lastX=0;
+
+     for($pixelIndex=0; $pixelIndex<$pixelsX; $pixelIndex++)
      {
         $rgb = imagecolorat($tmp,$pixelIndex,$lineIndex);
         $value = ($rgb >> 16) & 0xFF;
         if($value < $whiteLevel) //Nonwhite image parts
         {
-           if($firstX == 0)
-              $firstX = $pixelIndex;
-           
-           $lastX = $pixelIndex;         
+           if($firstX==0) {
+              $firstX=$pixelIndex;
+           }
+           if($firstX>0) {
+              $lastX = $pixelIndex; 
+           }        
         }
      }
+ 
+     
         
      $pixelIndex=$firstX;
+     $skipDwell = TRUE;
+     $workDone = FALSE;
      for($pixel=($offsetX+$firstX*$resX); $pixel < ($sizeX+$offsetX); $pixel+=$resX)
      {
           if($pixelIndex == $lastX) {
-             print(";<---\n"); 
+             //print(";<---\n"); 
+             $skipDwell = TRUE;
              break;
           }
-          if($pixelIndex == $firstX)
-             {            
-             print("G1 X".round($pixel+$overScan,4)." Y".round($line,4)." F$travelRate\n");
-             print("G1 F$feedRate\n");
-             print("G1 X".round($pixel,4)." Y".round($line,4)."\n");
-             }
-          else
-             print("G1 X".round($pixel,4)."\n");
 
           $rgb = imagecolorat($tmp,$pixelIndex,$lineIndex);
           $value = ($rgb >> 16) & 0xFF;
           $value = round(map($value,255,0,$laserMin,$laserMax),4);
-          print("$laserCMD$value\n");
+
+          if($pixelIndex == $firstX)
+             {  
+             $workDone = TRUE;
+             print("$laserOFFCmd$laserOFFPwr\n");                    
+             print("G0 X".round($pixel+$overScan,4)." Y".round($line,4)." F$travelRate\n");
+             print("$laserONCmd$value\n"); 
+             print("G1 F$feedRate\n");
+             print("G1 X".round($pixel,4)." Y".round($line,4)."\n");
+             }
+          else
+             {
+             print("$laserONCmd$value\n");
+             print("G1 X".round($pixel,4)."\n");
+             }
+
           $pixelIndex++;
       }
-      print("$laserCMD$laserOff ; Laser OFF\n");      
+      print("$laserOFFCmd$laserOFFPwr ; LASER OFF\n");
+      if ($skipDwell == TRUE && $workDone == TRUE) {
+      	print("G4 P8 ; Dwell on it\n");      
+      }
       $lineIndex++;
+     
   }
-  
+  print("G91 ;relative positioning\n");  // change to relative positioning for the Z axis
+  print("G1 Z-$passDepth\n");
+  print("G90 ;absolute positioning\n");  // put back to absolute positioning for Z axis
+  //print("M117 End of PASS $pass\n");
+  print("$laserOFFCmd$laserOFFPwr; MAKE SURE LASER IS OFF!\n");
+  print("G4 P2000 ;Pause for 2 seconds\n");  // set Dwell to 2 seconds to allow Laser time to settle off
 }
 $lineIndex--;
 
-print("$laserCMD$laserOff ;Turn laser off\n");
 print("G0 X$offsetX Y$offsetY F$travelRate ;Go home\n");
 print("G28");
 imagedestroy($tmp);
-imagedestroy($flip);
+imagedestroy($fliped);
 ?>
